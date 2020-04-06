91dba7eeea9977ae25d6cf987bd266af8ae7eb1b
blockinator <36169687+blockinator@users.noreply.github.com>
2020-04-06 11:43:28 -0700

    Update seed nodes

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -275,8 +275,8 @@ const static   boost::uuids::uuid CRYPTONOTE_NETWORK         =
 
 const char* const SEED_NODES[] = {
   "194.247.190.95:11897",
-  "ec2-18-224-43-204.us-east-2.compute.amazonaws.com:11897",
-  "ec2-18-222-184-3.us-east-2.compute.amazonaws.com:11897",
+  "95.217.153.70:11897",
+  "116.203.140.242:11897",
   "34.66.252.140:11897"
 };
 } // CryptoNote
