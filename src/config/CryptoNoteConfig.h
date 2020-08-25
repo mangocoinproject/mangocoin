87802a6b3156d15ab08fc9a1db3e4103185e40a5
Mangocoin <52526923+mangocoinproject@users.noreply.github.com>
2020-08-25 09:35:47 -0700

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const uint32_t MIXIN_LIMITS_V3_HEIGHT                        = 100000;
 const uint64_t DEFAULT_MIXIN_V0                              = 3;
 const uint64_t DEFAULT_MIXIN_V1                              = MAXIMUM_MIXIN_V1;
 const uint64_t DEFAULT_MIXIN_V2                              = MAXIMUM_MIXIN_V2;
-const uint64_t DEFAULT_MIXIN_V3                              = MINIMUM_MIXIN_V3;
+const uint64_t DEFAULT_MIXIN_V3                              = 0;
 
 const uint64_t DEFAULT_DUST_THRESHOLD                        = UINT64_C(10000);
 const uint64_t DEFAULT_DUST_THRESHOLD_V2                     = UINT64_C(0);
